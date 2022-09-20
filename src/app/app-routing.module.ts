73886322d818fec88a1856e717ab7ce46e4d1acd
karthik <karthiks9066@gmail.com>
2022-09-20 23:50:07 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import the components for navigation/routing purpose
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { LaptopComponent } from './laptop/laptop.component';
 
-const routes: Routes = [];
+//Configure the routing path for the above imported components
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'cards',component:CardsComponent},
+  {path:'laptop', component:LaptopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//accessing ngModel for 2-way binding
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CardsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
