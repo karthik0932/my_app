dbab18f03ddcfa55772ee081dbec8b7b974c446d
karthik <karthiks9066@gmail.com>
2022-09-29 11:41:29 +0530

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: true,
-   userapi:"http://localhost:3000/users",
-   cartapi:"http://localhost:3000/cart",
+   userapi:"https://heroku-json-server-shivu.herokuapp.com/users",
+   cartapi:"https://heroku-json-server-shivu.herokuapp.com/cart",
   
-  postapi:" http://localhost:3000/posts"
+  postapi:" https://heroku-json-server-shivu.herokuapp.com/posts"
 };
 
 /*
